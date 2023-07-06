5c68bbc36336d223afec0e0cdddc12aa06139c13
carlos731 <chtsca2001@gmail.com>
2023-07-06 07:20:06 -0300

    Realizando requisições pelo controller

--- lib/src/pages/auth/controller/auth_controller.dart
+++ lib/src/pages/auth/controller/auth_controller.dart
@@ -1,15 +1,19 @@
 import 'package:get/get.dart';
 
-class AuthController extends GetxController{
+import '../repository/auth_repository.dart';
 
+class AuthController extends GetxController {
   RxBool isLoading = false.obs;
+
+  final authRepository = AuthRepository();
 
   Future<void> signIn({required String email, required String password}) async {
     isLoading.value = true;
 
-    await Future.delayed(const Duration(seconds: 2));
-  
+    await authRepository.signIn(email: email, password: password);
+
+    //await Future.delayed(const Duration(seconds: 2)); // criado antes para fazer o delay de 2 segundos para duração do circular progress do botão!
+
     isLoading.value = false;
   }
-
 }
--- lib/src/pages/auth/repository/auth_repository.dart
+++ lib/src/pages/auth/repository/auth_repository.dart
@@ -16,8 +16,10 @@ class AuthRepository {
 
     if(result['result'] != null){
       print('Signin funcionou!');
+      print(result['result']);
     } else {
       print('Signin não funcionou!');
+      print(result['error']);
     }
 
   }
