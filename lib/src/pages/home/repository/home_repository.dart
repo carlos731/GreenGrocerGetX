8cc468362bea123836c3433b9e8850d7b2c11c19
carlos731 <chtsca2001@gmail.com>
2023-07-09 19:54:54 -0300

    Aula 97 - Finalizando o HomeRepository

--- lib/src/pages/home/repository/home_repository.dart
+++ lib/src/pages/home/repository/home_repository.dart
@@ -1,4 +1,6 @@
 import 'package:app/src/constants/endpoints.dart';
+import 'package:app/src/models/category_model.dart';
+import 'package:app/src/pages/home/result/home_result.dart';
 import 'package:app/src/services/http_manager.dart';
 
 class HomeRepository {
@@ -10,10 +12,17 @@ class HomeRepository {
       method: HttpMethods.post,
     );
 
-    if(result['result'] != null) {
+    if (result['result'] != null) {
       // Lista
+      List<CategoryModel> data =
+          (result['result'] as List<Map<String, dynamic>>)
+              .map(CategoryModel.fromJson)
+              .toList();
+
+      return HomeResult<CategoryModel>.success(data);
     } else {
       // Erro
+      return HomeResult.error("Ocorreu um erro inesperado ao recuperar as categorias");
     }
   }
 }
