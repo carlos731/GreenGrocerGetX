410b5c8f75abb0c26d717a03ef94099749fc677d
carlos731 <chtsca2001@gmail.com>
2023-07-14 09:50:34 -0300

    Aula 128 - Observação sobre a data de criação e de vencimento do pagamento PIX

--- lib/src/services/utils_services.dart
+++ lib/src/services/utils_services.dart
@@ -40,7 +40,7 @@ class UtilsServices {
   String formatDateTime(DateTime dateTime) {
     initializeDateFormatting();
     DateFormat dateFormat = DateFormat.yMd('pt_BR').add_Hm();
-    return dateFormat.format(dateTime);
+    return dateFormat.format(dateTime.toLocal());
   }
 
   // Método usando fluttertoast
